bf5babd4b2138c7eaac096b8f5fb93002d17473c
Miles Cobb <milescobb@miless-air.lan>
2023-06-28 17:54:09 -0700

    Updates

---
+++ precache-manifest.84dce2d42fe2894f135fbda2988d0855.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f1f705025b7e8ef7c77",
+    "url": "/photography/static/css/main.aaf18f24.chunk.css"
+  },
+  {
+    "revision": "8f1f705025b7e8ef7c77",
+    "url": "/photography/static/js/main.8f1f7050.chunk.js"
+  },
+  {
+    "revision": "205849d973eaa941ab0f",
+    "url": "/photography/static/js/1.205849d9.chunk.js"
+  },
+  {
+    "revision": "1275f2aa5f0fa7ebfab4",
+    "url": "/photography/static/js/2.1275f2aa.chunk.js"
+  },
+  {
+    "revision": "fc6e39d768661f351fcf",
+    "url": "/photography/static/js/runtime~main.fc6e39d7.js"
+  },
+  {
+    "revision": "c673f1bd7106775b5ebee40d17c71750",
+    "url": "/photography/static/media/cooper-hewitt.light.c673f1bd.otf"
+  },
+  {
+    "revision": "f9e93a40b9e87142988bcf8b0b5618ec",
+    "url": "/photography/index.html"
+  }
+];
