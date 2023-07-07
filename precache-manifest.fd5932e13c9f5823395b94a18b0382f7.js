288b7f18a8ea2203c246b211c3b5e798ed644519
Miles Cobb <milescobb@Miless-MacBook-Air.local>
2023-07-06 23:17:03 -0700

    Updates

---
+++ precache-manifest.fd5932e13c9f5823395b94a18b0382f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9da387b7b9da9e55d19a",
+    "url": "/static/css/main.9034af02.chunk.css"
+  },
+  {
+    "revision": "9da387b7b9da9e55d19a",
+    "url": "/static/js/main.9da387b7.chunk.js"
+  },
+  {
+    "revision": "f09d6da400d0c14a7f1d",
+    "url": "/static/js/1.f09d6da4.chunk.js"
+  },
+  {
+    "revision": "8bba0af168a041bff12f",
+    "url": "/static/js/2.8bba0af1.chunk.js"
+  },
+  {
+    "revision": "0e85d9ea22f618c5f874",
+    "url": "/static/js/runtime~main.0e85d9ea.js"
+  },
+  {
+    "revision": "c673f1bd7106775b5ebee40d17c71750",
+    "url": "/static/media/cooper-hewitt.light.c673f1bd.otf"
+  },
+  {
+    "revision": "bd2695c4d00d7c0d627caba16a76aaaf",
+    "url": "/index.html"
+  }
+];
